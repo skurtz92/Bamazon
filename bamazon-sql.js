561a7f04d9c97a87ba3381e4741382f7f012dcad
Sydney Kurtz <sydney.kurtz@yahoo.com>
2016-10-21 20:54:18 -0500

    first update

---
+++ bamazon-sql.js
@@ -0,0 +1,22 @@
+#CREATE DATABASE Bamazon;
+
+CREATE TABLE Bamazon.Products (
+	ItemID INTEGER(10) AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(10),
+    DepartmentName VARCHAR(10),
+    Price INTEGER(10),
+    StockQuantity INTEGER(10),
+    PRIMARY KEY (ItemID));
+    
+INSERT INTO Bamazon.Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Toothpaste", "Walmart", 1.50, 4), 
+			("Shampoo", "Target", 6.50, 5),
+            ("Cheese", "Grocery", 1.50, 6),
+            ("Carrots", "Grocery", 4.00, 9),
+            ("Nike", "Sporting goods", 49.99, 4),
+            ("Tshirt", "Clothes", 10.00, 7),
+            ("Earrings", "Accessories", 9.99, 5),
+            ("Basketball", "Equipment", 4.99, 7),
+            ("Tennis racket", "Sports", 9.99, 2),
+            ("Wireless Keyboard", "Computers", 3.99, 1);
+            SELECT * FROM Bamazon.Products;
